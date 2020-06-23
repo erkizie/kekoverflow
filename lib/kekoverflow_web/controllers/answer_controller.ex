d0ed6be280d949e052262e49894dfb8bf9a7bfc0
Erkizie <erkizie@gmail.com>
2020-06-23 20:25:23 +0300

    Updated Answers controller for create function

--- lib/kekoverflow_web/controllers/answer_controller.ex
+++ lib/kekoverflow_web/controllers/answer_controller.ex
@@ -1,40 +1,29 @@
 defmodule KekoverflowWeb.AnswerController do
   use KekoverflowWeb, :controller
+  import Ecto
+  require IEx
 
-  alias Kekoverflow.Answers
-  alias Kekoverflow.Answers.Answer
+  alias Kekoverflow.{Repo, Answers.Answer, Answers, Questions, Questions.Question}
 
-  def index(conn, _params) do
-    answers = Answers.list_answers()
-    render(conn, "index.html", answers: answers)
-  end
+  def create(conn, %{"answer" => answer_params, "question_id" => question_id}) do
+    user = conn.assigns.current_user
+    question = Repo.get!(Question, question_id) |> Repo.preload([:user, :answers])
 
-  def new(conn, _params) do
-    changeset = Answers.change_answer(%Answer{})
-    render(conn, "new.html", changeset: changeset)
-  end
+#    changeset = question
+#      |> Ecto.build_assoc(:answers)
+#      |> Answer.changeset(answer_params)
 
-  def create(conn, %{"answer" => answer_params}) do
-    case Answers.create_answer(answer_params) do
-      {:ok, answer} ->
+    changeset = Answer.changeset(%Answer{user_id: user.id, question_id: String.to_integer(question_id)}, answer_params)
+
+    case Repo.insert(changeset) do
+      {:ok, _answer} ->
         conn
         |> put_flash(:info, "Answer created successfully.")
-        |> redirect(to: Routes.answer_path(conn, :show, answer))
+        |> redirect(to: Routes.question_path(conn, :show, question_id))
 
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
+      {:error, changeset} ->
+        render(conn, KekoverflowWeb.QuestionView, "show.html", question: question, comment_changeset: changeset)
     end
-  end
-
-  def show(conn, %{"id" => id}) do
-    answer = Answers.get_answer!(id)
-    render(conn, "show.html", answer: answer)
-  end
-
-  def edit(conn, %{"id" => id}) do
-    answer = Answers.get_answer!(id)
-    changeset = Answers.change_answer(answer)
-    render(conn, "edit.html", answer: answer, changeset: changeset)
   end
 
   def update(conn, %{"id" => id, "answer" => answer_params}) do
@@ -44,7 +33,7 @@ defmodule KekoverflowWeb.AnswerController do
       {:ok, answer} ->
         conn
         |> put_flash(:info, "Answer updated successfully.")
-        |> redirect(to: Routes.answer_path(conn, :show, answer))
+        |> redirect(to: Routes.question_answer_path(conn, :show, answer))
 
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "edit.html", answer: answer, changeset: changeset)
@@ -57,6 +46,6 @@ defmodule KekoverflowWeb.AnswerController do
 
     conn
     |> put_flash(:info, "Answer deleted successfully.")
-    |> redirect(to: Routes.answer_path(conn, :index))
+    |> redirect(to: Routes.question_answer_path(conn, :index))
   end
 end
