2bb462a49e0260e46913803157e2897823edc758
Erkizie <erkizie@gmail.com>
2020-07-29 17:30:07 +0300

    Updated factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,12 +1,18 @@
 defmodule Kekoverflow.Factory do
   use ExMachina.Ecto, repo: Kekoverflow.Repo
-  alias Kekoverflow.{Questions.Question, Questions, Answers.Answer, Answers, Comments.Comment, Comments, Users.User}
+  alias Kekoverflow.{Questions.Question, Questions.Tag, Questions, Answers.Answer, Answers, Comments.Comment, Comments, Users.User}
 
   def user_factory do
     %User{
       email: Faker.Internet.email(),
-      role: "user",
-      password: "qwerty123"
+      role: "user"
+    }
+  end
+
+  def admin_factory do
+    %User{
+      email: Faker.Internet.email(),
+      role: "admin"
     }
   end
 
@@ -23,8 +29,59 @@ defmodule Kekoverflow.Factory do
     %Answer{
       body: Faker.Lorem.Shakespeare.En.romeo_and_juliet(),
       rate: 1,
+      user: build(:user)
+    }
+  end
+
+  def comment_factory do
+    %Comment{
+      body: Faker.Lorem.Shakespeare.En.romeo_and_juliet(),
+      user: build(:user)
+    }
+  end
+
+  def comment_for_question_factory do
+    %Comment{
+      body: Faker.Lorem.Shakespeare.En.romeo_and_juliet(),
       user: build(:user),
       question: build(:question)
+    }
+  end
+
+  def comment_for_answer_factory do
+    %Comment{
+      body: Faker.Lorem.Shakespeare.En.romeo_and_juliet(),
+      user: build(:user),
+      question: build(:question),
+      answer: build(:answer)
+    }
+  end
+
+  def tag_factory do
+    %Tag{
+      text: "elixir",
+      questions: []
+    }
+  end
+
+  def answer_with_assoc_factory do
+    %Answer{
+      body: Faker.Lorem.Shakespeare.En.romeo_and_juliet(),
+      rate: 1,
+      user: build(:user),
+      question: build(:question)
+    }
+  end
+
+  def question_with_assoc_factory do
+    %Question{
+      title: Faker.Superhero.name(),
+      body: Faker.Lorem.Shakespeare.hamlet(),
+      rate: 2,
+      user: build(:user),
+      answers: [build(:answer)],
+      tags: [],
+      comments: []
     }
   end
 end
